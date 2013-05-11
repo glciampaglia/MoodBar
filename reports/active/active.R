ecce8a268f88d562b2af0065cab6c8add65944f0
Giovanni Luca Ciampaglia <ciampagg@usi.ch>
2013-05-11 18:35:00 -0400

    Now plotting retained % over 30-day periods

--- reports/active/active.R
+++ reports/active/active.R
@@ -1,14 +1,16 @@
 require(ggplot2)
-active <- read.table("../../data/active.tsv", stringsAsFactors=T, sep="\t",
+active <- read.table("../../data/active_30.tsv", stringsAsFactors=T, sep="\t",
                      header=T)
 active.tot <- read.table("../../data/active_total.tsv", stringsAsFactors=T,
                          header=T, sep="\t")
 
+active <- subset(active, group != 'historical')
+
 for (group in levels(active$group)) {
     tot <- active.tot[active.tot$uw_group == group,]$total
     idx <- active$group == group
-    active[idx,]$active <- active[idx,]$active / tot
+    active[idx,]$retained <- active[idx,]$retained / tot
 }
 
-ggplot(subset(active, group != 'historical'), aes(age, active, colour=group)) + geom_point() +
-scale_y_continuous(limits=c(0,0.1)) + scale_y_log10() #+ scale_x_continuous(limits=c(200,300))
+ggplot(active, aes(period, retained, colour=group)) + geom_point() + 
+scale_y_log10() + scale_y_continuous(limits=c(0,0.05)) 
