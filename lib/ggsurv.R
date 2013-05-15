19b2cd1a965b23cc86f181454b22c5cc1765f9ed
Giovanni Luca Ciampaglia <ciampagg@usi.ch>
2013-05-14 22:37:19 -0400

    Checking for strata, added surv2df utility function to ggsurv.R

--- lib/ggsurv.R
+++ lib/ggsurv.R
@@ -9,26 +9,56 @@
 # * add marks for censored observations
 # * pass xlab, ylab, main ...
 
-require(ggplot2)
+surv2df <- function(sfit) {
+  if (is.null(sfit$strata)) {
+    tmp.frame <- data.frame(
+      time = sfit$time,
+      n.risk = sfit$n.risk,
+      n.event = sfit$n.event,
+      surv = sfit$surv,
+      upper = sfit$upper,
+      lower = sfit$lower
+      )
+  } else {
+    # define tmp frame 
+    tmp.frame <- data.frame(
+      time = sfit$time,
+      n.risk = sfit$n.risk,
+      n.event = sfit$n.event,
+      surv = sfit$surv,
+      strata = factor(summary(sfit, censored = T)$strata),
+      upper = sfit$upper,
+      lower = sfit$lower
+      )
+  }
+  tmp.frame
+}
 
-ggsurv <- function(sfit, ystratalabs = NULL, ystrataname = NULL, ...) 
+ggsurv <- function(sfit, ...) 
 {
 
-  if(is.null(ystratalabs)) ystratalabs <- as.character(levels(summary(sfit)$strata))
-  if(is.null(ystrataname)) ystrataname <- "Strata"
-
-  tmp.frame <- data.frame(
-    time = sfit$time,
-    n.risk = sfit$n.risk,
-    n.event = sfit$n.event,
-    surv = sfit$surv,
-    strata = factor(summary(sfit, censored = T)$strata),
-    upper = sfit$upper,
-    lower = sfit$lower
-  )
+  require(ggplot2)
   
-  p <- ggplot(tmp.frame, aes(time, surv, group = strata), ...) +
-    geom_step(aes(linetype = strata))
+  tmp.frame <- surv2df(sfit)
+  
+  if (is.null(sfit$strata)) { # survfit object has no strata
+
+    p <- ggplot(tmp.frame, aes(time, surv), ...) + geom_step()
+
+  } else { # survfit object has strata
+  
+    # extract strata name and labels from survfit object
+    strata.levels <- as.character(levels(summary(sfit)$strata))
+    strata.name <- unlist(strsplit(strata.levels[1], "="))[1]
+    strata.labels <- vector(mode = "character", 0)
+    for (i in 1:length(strata.levels)) {
+      strata.labels[i] <- unlist(strsplit(strata.levels[i], "="))[2]
+    }
+    
+    levels(tmp.frame$strata) <- strata.labels
+    p <- ggplot(tmp.frame, aes(time, surv, group = strata), ...) +
+      geom_step(aes(linetype = strata)) + labs(linetype = strata.name)
+  }
   
   print(p)
 }
